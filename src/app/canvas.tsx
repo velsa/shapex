96305f7fb133aa1abd6d0ca25daad8b57bd70eed
velsa <velshome@yahoo.com>
2024-03-31 19:31:17 +0300

    drawLine with useCallback, support darkMode

--- src/app/canvas.tsx
+++ src/app/canvas.tsx
@@ -1,10 +1,46 @@
-import { MutableRefObject, useEffect, useRef } from "react";
+import {
+  MutableRefObject,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import { useMediaQuery } from "react-responsive";
 import { ShapeX, ShapeXPoint } from "./shapex";
 
 type DrawStyle = { color?: string; width?: number };
 
 const Canvas = (props: { shape: ShapeX }) => {
   const canvasRef: MutableRefObject<HTMLCanvasElement | null> = useRef(null);
+  const systemPrefersDark = useMediaQuery(
+    {
+      query: "(prefers-color-scheme: dark)",
+    },
+    undefined,
+    (prefersDark) => {
+      setIsDark(prefersDark);
+    }
+  );
+  const [isDark, setIsDark] = useState(systemPrefersDark);
+  const drawLine = useCallback(
+    (
+      ctx: CanvasRenderingContext2D,
+      from: ShapeXPoint,
+      to: ShapeXPoint,
+      style: DrawStyle = {}
+    ) => {
+      const { color = "black", width = 1 } = style;
+
+      ctx.beginPath();
+      ctx.moveTo(from.x, from.y);
+      ctx.lineTo(to.x, to.y);
+      ctx.strokeStyle = isDark ? "white" : "black";
+      ctx.lineWidth = width;
+      ctx.stroke();
+      // ctx.strokeText(`${from.x}, ${from.y}`, from.x, from.y);
+    },
+    [isDark]
+  );
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -32,26 +68,9 @@ const Canvas = (props: { shape: ShapeX }) => {
     ) {
       drawLine(ctx, current, current.next || props.shape.head);
     }
-  }, [props.shape]);
+  }, [drawLine, props.shape]);
 
   return <canvas ref={canvasRef} {...props} width={288} height={480} />;
-};
-
-const drawLine = (
-  ctx: CanvasRenderingContext2D,
-  from: ShapeXPoint,
-  to: ShapeXPoint,
-  style: DrawStyle = {}
-) => {
-  const { color = "black", width = 1 } = style;
-
-  ctx.beginPath();
-  ctx.moveTo(from.x, from.y);
-  ctx.lineTo(to.x, to.y);
-  ctx.strokeStyle = color;
-  ctx.lineWidth = width;
-  ctx.stroke();
-  // ctx.strokeText(`${from.x}, ${from.y}`, from.x, from.y);
 };
 
 export default Canvas;
